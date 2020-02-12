f89b818ca860dab6d1ad9c510fa5671a31c7284e
kekzdealer <kekzdealer@gmail.com>
2020-02-12 02:01:30 +0100

    logging now has [HH:mm:ss] timestamps

--- src/utility/Logger.java
+++ src/utility/Logger.java
@@ -1,13 +1,23 @@
 package utility;
 
 import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Logger {
+	
+	private static String getTimeStamp() {
+		final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		
+		return new StringBuilder()
+				.append("[").append(dtf.format(now)).append("]").toString();
+	}
 	
 	public static class INFO {
 		
 		public static void log(String msg) {
-			System.out.println("INFO: " + msg);
+			System.out.println(getTimeStamp() + " INFO: " + msg);
 		}
 		
 		public static PrintStream getPrintStream() {
@@ -18,7 +28,7 @@ public class Logger {
 	public static class ERROR {
 		
 		public static void log(String msg) {
-			System.err.println("ERROR: " + msg);
+			System.err.println(getTimeStamp() + " ERROR: " + msg);
 		}
 		
 		public static PrintStream getPrintStream() {
